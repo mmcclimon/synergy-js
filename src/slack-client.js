aa6daad741a216bc239bcdf7320348d59d828419
Michael McClimon <michael@mcclimon.org>
2020-03-08 13:17:44 -0400

    Do a bunch of slack connection bootstrapping

--- src/slack-client.js
+++ src/slack-client.js
@@ -3,6 +3,15 @@ const http = require('superagent');
 const WebSocket = require('ws');
 const Logger = require('./logger.js');
 
+const intoObject = function(arr, key, val) {
+  return arr.reduce((acc, el) => {
+    const k = el[key];
+    const v = val ? el[val] : el;
+    acc[k] = v;
+    return acc;
+  }, {});
+};
+
 const SlackClient = class {
   constructor(apiKey) {
     if (typeof apiKey === 'undefined') {
@@ -15,6 +24,11 @@ const SlackClient = class {
     this.teamData = null;
     this.ws = null;
     this.connected = false;
+
+    this.users = null;
+    this.channels = null;
+    this.dmChannels = null;
+    this.groupConversations = null;
   }
 
   connect() {
@@ -33,8 +47,6 @@ const SlackClient = class {
       throw new Error(`Couldn't connect to slack RTM: ${data.error}`);
     }
 
-    Logger.info('Connected to Slack!');
-
     this.ourName = data.self.name;
     this.ourId = data.self.id;
     this.teamData = data.team;
@@ -50,7 +62,87 @@ const SlackClient = class {
     this.ws.on('close', () => clearInterval(interval));
   }
 
-  setUp() {}
+  setUp() {
+    Logger.info('Connected to Slack!');
+    this.loadUsers();
+    this.loadChannels();
+    this.loadGroupConversations();
+    this.loadDMs();
+  }
+
+  _apiUrl(method) {
+    return `https://slack.com/api/${method}`;
+  }
+
+  get _apiAuthHeader() {
+    return `Bearer ${this.apiKey}`;
+  }
+
+  apiCall(method, data) {
+    let postType = 'json';
+
+    if (data._formEncoded) {
+      delete data._formEncoded;
+      postType = 'form';
+    }
+
+    return http
+      .post(this._apiUrl(method))
+      .type(postType)
+      .set('Authorization', this._apiAuthHeader)
+      .send(data)
+      .then(res => {
+        if (res.error) {
+          return Promise.reject(
+            `error talking to slack during method ${method}: ${res.text}`
+          );
+        }
+
+        return Promise.resolve(res.body);
+      });
+  }
+
+  async loadUsers() {
+    const data = await this.apiCall('users.list', { presence: false });
+    this.users = intoObject(data.members, 'id');
+
+    // fix up ourselves, because sometimes the name isn't right.
+    this.users[this.ourId].name = this.ourName;
+
+    Logger.info('Slack users loaded');
+  }
+
+  async loadChannels() {
+    const data = await this.apiCall('conversations.list', {
+      _formEncoded: true,
+      excludeArchived: true,
+      types: 'public_channel'
+    });
+
+    this.channels = intoObject(data.channels, 'id');
+    Logger.info('Slack channels loaded');
+  }
+
+  async loadGroupConversations() {
+    const data = await this.apiCall('conversations.list', {
+      _formEncoded: true,
+      types: 'mpim,private_channel'
+    });
+
+    this.groupConversations = intoObject(data.channels, 'id');
+    Logger.info('Slack group conversations loaded');
+  }
+
+  async loadDMs() {
+    const data = await this.apiCall('conversations.list', {
+      _formEncoded: true,
+      excludeArchived: true,
+      types: 'im'
+    });
+
+    this.dmChannels = intoObject(data.channels, 'user', 'id');
+    Logger.info('Slack DM channels loaded');
+  }
 };
 
 module.exports = SlackClient;
