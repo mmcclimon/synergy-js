44c2cc7d4b239359b9edfdea1b7e9690532c67c3
Michael McClimon <michael@mcclimon.org>
2020-03-13 17:51:51 -0400

    Improve clox reactor time handling

--- src/reactors/clox.ts
+++ src/reactors/clox.ts
@@ -1,3 +1,5 @@
+import * as moment from 'moment-timezone';
+
 import BaseReactor from './base';
 import Commando from '../commando';
 
@@ -16,8 +18,35 @@ Commando.registerListener('clox', {
   handler: function(event) {
     event.markHandled();
 
-    const now = new Date();
+    // TODO: get zones from user directory
+    const tzNames = ['America/New_York', 'Australia/Sydney', 'Etc/UTC'];
 
-    event.reply(now);
+    // TODO: clox at not-now
+    const now = moment();
+
+    const tzmap = Object.fromEntries(
+      tzNames.map(tz => [tz, moment.tz.zone(tz).utcOffset(now)])
+    );
+
+    const sorted = tzNames.slice().sort((a, b) => tzmap[a] - tzmap[b]);
+    const resp = [];
+
+    sorted.forEach(zone => {
+      const offset = now
+        .clone()
+        .tz(zone)
+        .utcOffset();
+
+      const targetTime = now
+        .clone()
+        .utc()
+        .add(offset, 'minutes');
+
+      const pretty = targetTime.format('ddd, HH:mm');
+
+      resp.push(`${moment.tz.zone(zone).abbr(now)}: ${pretty}`);
+    });
+
+    event.reply(resp.join('\n'));
   },
 });
